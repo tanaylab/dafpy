63c7af83e40653449cd3cd85072b2a0e9d36af80
Oren Ben-Kiki <oren@ben-kiki.org>
2024-03-23 14:52:22 +0200

    Change default policy to warn on inefficient operations.

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -9,5 +9,5 @@ from daf import *
 
 
 def test_generic() -> None:  # pylint: disable=too-many-statements
-    assert inefficient_action_handler("ErrorHandler") == "IgnoreHandler"
-    assert inefficient_action_handler("IgnoreHandler") == "ErrorHandler"
+    assert inefficient_action_handler("ErrorHandler") == "WarnHandler"
+    assert inefficient_action_handler("WarnHandler") == "ErrorHandler"
