475c69377f7d6675b5a124fed20bae135b827076
orenbenkiki <oren@ben-kiki.org>
2026-08-18 13:57:29 +0300

    Fix sphinx HTML theme.

--- docs/conf.py
+++ docs/conf.py
@@ -110,9 +110,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-import sphinx_rtd_theme
+# Setting ``html_theme_path`` from ``sphinx_rtd_theme.get_html_theme_path`` is deprecated and warns,
+# and the theme is found without it.
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
